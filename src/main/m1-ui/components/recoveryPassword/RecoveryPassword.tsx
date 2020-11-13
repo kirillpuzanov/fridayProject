cbbc8d99d753846a9beaa4e8f375e5a09aceead8
kirillpuzanov <kirillpuzanov@mail.ru>
2020-11-13 22:20:00 +0300

    test 3

--- src/main/m1-ui/components/recoveryPassword/RecoveryPassword.tsx
+++ src/main/m1-ui/components/recoveryPassword/RecoveryPassword.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 export const RecoveryPassword: React.FC = () => {
 
     return <section>
+        <h1>RecoveryPassword</h1>
+        <span>Forgot your password?</span>
+        <div>
+            <span>Enter your mail for </span>
+            <input/>
+            <span></span>
+            <span></span>
+        </div>
         RecoveryPassword
 
     </section>
