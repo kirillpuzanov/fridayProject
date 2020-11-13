0d9afd6caf62c1afa1c2e588b8fbcdc3ece8b99e
Andrei <stigmaby@gmail.com>
2020-11-13 22:06:11 +0300

    Test commit

--- src/main/m1-ui/components/recoveryPassword/RecoveryPassword.tsx
+++ src/main/m1-ui/components/recoveryPassword/RecoveryPassword.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 export const RecoveryPassword: React.FC = () => {
 
     return <section>
-        RecoveryPassword
-
+        <h1>RecoveryPassword</h1>
+        <span>Forgot your password?</span>
+        <div>
+            <span>Enter your mail for </span>
+            <input/>
+        </div>
     </section>
 }
