3580b5341d8bf43f3f52064d429a45bbab95b7a5
omgpiu <dyba@List.ru>
2020-11-13 20:53:39 +0300

    Login span

--- src/main/m1-ui/components/signIn/SignIn.tsx
+++ src/main/m1-ui/components/signIn/SignIn.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 
 export const SignIn: React.FC = () => {
 
-    return <section>
+    return <>
+        <span>Hello my dear friend</span>
+        <input type="text" placeholder={'Enter your name'}/>
+        <input type="text" placeholder={'Enter your email'}/>
+        <button>SignIn</button>
         <div>SignIn</div>
-    </section>
+    </>
 }
