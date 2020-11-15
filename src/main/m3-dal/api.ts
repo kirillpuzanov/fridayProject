f39051d88033a07fe2b00f802b1fb5eadccc39f6
kirillpuzanov <kirillpuzanov@mail.ru>
2020-11-15 10:32:07 +0300

    test//

--- src/main/m3-dal/api.ts
+++ src/main/m3-dal/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-
-const instance = axios.create({
-        baseURL: 'https://'
+export const instance = axios.create({
+    //локально:
+    baseURL: 'http://localhost:7542/2.0/',
+    // gh-pages:
+    // baseURL: 'https://neko-back.herokuapp.com/2.0/'
+    withCredentials: true,
 })
 
 export const profileAPI = {
@@ -13,6 +16,27 @@ export const profileAPI = {
 }
 
 export const authAPI = {
+    login(data:LoginParamsType){
+        return instance.post<ResponseType>('auth/login',data)}
+}
 
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe: boolean
+
+}
+export type ResponseType = {
+    _id:string
+    email:string
+    name:string
+    avatar?:string
+    publicCardPacksCount:number
+    created:Date
+    updated:Date
+    isAdmin:boolean
+    verified:boolean
+    rememberMe:boolean
+    error:string
 
 }
