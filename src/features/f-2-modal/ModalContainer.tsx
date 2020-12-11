355556418370a2b5b49be5034b7ebf193f6ca6bb
omgpiu <dyba@List.ru>
2020-12-11 19:13:21 +0300

    Change input to textarea

--- src/features/f-2-modal/ModalContainer.tsx
+++ src/features/f-2-modal/ModalContainer.tsx
@@ -1,6 +1,7 @@
 import React, {ChangeEvent, useEffect, useState} from 'react';
 import {MyInput} from '../../main/common/myComponent/myInput/MyInput';
 import {MyModal} from '../../main/common/modal/MyModal';
+import {MyTextArea} from '../../main/common/myComponent/myTextArea/MyInput';
 
 type ContainerType = {
     isOpen: boolean
@@ -22,67 +23,66 @@ const ModalContainer: React.FC<ContainerType> = ({
 
                                                  }) => {
 
-        const [itemName, setItemName] = useState<string | undefined>('');
-        const [updateItem, setUpdateItem] = useState<string | undefined>('');
+    const [itemName, setItemName] = useState<string | undefined>('');
+    const [updateItem, setUpdateItem] = useState<string | undefined>('');
+    useEffect(() => {
+        setUpdateItem(updateAnswer);
+    }, [updateAnswer]);
+    // useEffect(() => {
+    //     return () => {
+    //         setUpdateItem('');
+    //     };
+    // }, []);
+    useEffect(() => {
+        setItemName(updateItemName);
+    }, [updateItemName]);
+    // useEffect(() => {
+    //     return () => {
+    //         setItemName('');
+    //     };
+    // }, []);
 
-        useEffect(() => {
-            setUpdateItem(updateAnswer);
-        }, [updateAnswer]);
-        // useEffect(() => {
-        //     return () => {
-        //         setUpdateItem('');
-        //     };
-        // }, []);
-        useEffect(() => {
-            setItemName(updateItemName);
-        }, [updateItemName]);
-        // useEffect(() => {
-        //     return () => {
-        //         setItemName('');
-        //     };
-        // }, []);
 
+    const handleSubmit = () => {
+        changePack(itemName, updateItem);
+        setItemName('');
+        setUpdateItem('');
+        closeModal();
+    };
 
-        const handleSubmit = () => {
-            changePack(itemName, updateItem);
-            setItemName('');
-            setUpdateItem('');
-            closeModal();
-        };
+    const handleCancel = () => {
+        closeModal();
+    };
+    const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setItemName(e.currentTarget.value);
+    };
+    const updateInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setUpdateItem(e.currentTarget.value);
+    };
 
-        const handleCancel = () => {
-            closeModal();
-        };
-        const inputChange = (e: ChangeEvent<HTMLInputElement>) => {
-            setItemName(e.currentTarget.value);
-        };
-        const updateInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-            setUpdateItem(e.currentTarget.value);
-        };
-
-        return (
-            <>
-                {isOpen &&
-                <MyModal title={title}
-                         onCancel={handleCancel}
-                         onSubmit={handleSubmit}
-                         itemName={itemName}
-                         buttonName={buttonName}
-                >
-                    {buttonName !== 'DELETE' ?
+    return (
+        <>
+            {isOpen &&
+            <MyModal title={title}
+                     onCancel={handleCancel}
+                     onSubmit={handleSubmit}
+                     itemName={itemName}
+                     buttonName={buttonName}
+            >
+                {buttonName !== 'DELETE' ?
+                    <div>
+                        <MyInput onChange={inputChange} type={'text'} value={itemName} placeholder='Enter Title..'/>
+                        {itemName && itemName.length < 2 ? 'The length of the deck name must be at least 2 characters' : null}
                         <div>
-                            <MyInput onChange={inputChange} type={'text'} value={itemName} placeholder='Enter Title..'/>
-                            {/*{itemName.length < 2 ? 'The length of the deck name must be at least 2 characters' : null}*/}
-                            <div>
-                                <MyInput onChange={updateInputChange} type={'text'} value={updateItem}
-                                         placeholder='Enter Title..'/>
-                                {/*{updateItem.length < 2 ? 'The length of the deck name must be at least 2 characters' : null}*/}
-                            </div>
-                        </div> : null}
-                </MyModal>
-                }
-            </>
-        );
-    }
+                            <MyTextArea onChange={updateInputChange} value={updateItem}/>
+                            {updateItem && updateItem.length < 3 ? 'The length of the deck name must be at least 2 characters' : null}
+                        </div>
+                    </div> : null}
+
+            </MyModal>
+            }
+        </>
+    );
+};
 ;
 export default ModalContainer;
